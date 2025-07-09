f31b175991e0cad53cb6e1148db66e516608daa3
Dmitrii K <35343001+DmitriK10@users.noreply.github.com>
2025-07-09 16:29:56 +0300

    Delete op01_5.py

--- op01_5.py
+++
@@ -1,7 +0,0 @@
-#5. Написать конвертер валюты. Эта программа будет конвертировать введённую пользователем сумму в другую валюту. Курс валюты можно ввести самостоятельно и один раз.
-exchange_rate = 90.5  # Курс доллара к рублю
-usd_amount = float(input("Введите сумму в USD: "))
-
-rub_amount = usd_amount * exchange_rate
-print(f"{usd_amount} USD = {rub_amount:.2f} RUB")
-print();
