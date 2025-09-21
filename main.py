f9db89d79d89a792621ebd9c266b39b501ff3e48
Dmitrii K <ymd@yandex.ru>
2025-09-21 17:06:20 +0300

    create main.py in dev
    Добавил функцию area_rectangle в ветке dev

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# main.py в ветке dev
 def area_circle(radius):
     """Вычисляет площадь круга."""
     return 3.14159 * radius ** 2
 
+def area_rectangle(length, width):
+    """Вычисляет площадь прямоугольника."""
+    return length * width
+
 # Для проверки функций потом
 if __name__ == "__main__":
     print(f"Площадь круга с радиусом 5: {area_circle(5)}")
+    print(f"Площадь прямоугольника 4x6: {area_rectangle(4, 6)}")
